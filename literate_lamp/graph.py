57e62c992b34810f6742ea712ba866998ac072b0
Italo Silva <italo@maleldil.com>
2019-09-05 17:03:58 -0300

    Adds `graph.py`. It generates a Graph of the models.
    Graph is manually generated based on the perceived hierarchy.
    This is not directly related to the code (althoug the classes
    are defined here after all), but I found it easier to do this
    in code rather than draw it somewhere else. As such, I'm
    including it with the rest of the code.

---
+++ literate_lamp/graph.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import graphviz
+
+
+def main() -> None:
+    graph = graphviz.Digraph(
+        graph_attr={'splines': 'false'}
+    )
+
+    graph.edge("SimpleBase", "AttentiveReader", label="+Seq2Seq\n+Attn.")
+    graph.edge("AttentiveReader", "ZeroTriAN", label="+Self Attn.\n"
+                                                     "+Sequence Attn.")
+    graph.edge("ZeroTriAN", "TriAN",
+               label="+Word Overlap\n+Relation Emb.",
+               xlabel="+POS Emb.\n+NER Emb.\n")
+
+    graph.edge("SimpleBERT", "HierarchicalBERT", label="+LSTM\n+Attn.")
+    graph.edge("HierarchicalBERT", "RelationalBERT",
+               xlabel="+Rel. Emb.")
+    graph.edge("HierarchicalBERT", "BERT + RTM",
+               label="+Rel. Transf.")
+
+    graph.edge("SimpleXL", "AdvancedXL", "+LSTM\n+Attn.")
+    graph.edge("AdvancedXL", "RelationalXL", xlabel="+Rel. Emb.")
+    graph.edge("AdvancedXL", "XL + RTM", label="+Rel. Transf.")
+
+    graph.render('models', view=True)
+
+
+
+if __name__ == '__main__':
+    main()
