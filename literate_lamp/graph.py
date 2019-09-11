a200b9d204a29817a3fb04b6390d0692b2a27656
Italo Silva <italo@maleldil.com>
2019-09-10 21:35:12 -0300

    Includes the removed components in the model graph

--- literate_lamp/graph.py
+++ literate_lamp/graph.py
@@ -7,20 +7,23 @@ def main() -> None:
         graph_attr={'splines': 'false'}
     )
 
-    graph.edge("SimpleBase", "AttentiveReader", label="+Seq2Seq\n+Attn.")
+    graph.edge("SimpleBase", "AttentiveReader", label="+Seq2Seq\n"
+                                                      "+Bilinear Attn.")
     graph.edge("AttentiveReader", "ZeroTriAN", label="+Self Attn.\n"
                                                      "+Sequence Attn.")
     graph.edge("ZeroTriAN", "TriAN",
                label="+Word Overlap\n+Relation Emb.",
                xlabel="+POS Emb.\n+NER Emb.\n")
 
-    graph.edge("SimpleBERT", "HierarchicalBERT", label="+LSTM\n+Attn.")
+    graph.edge("SimpleBERT", "HierarchicalBERT", label="+LSTM\n+Attn.",
+               xlabel="-Pooler")
     graph.edge("HierarchicalBERT", "RelationalBERT",
                xlabel="+Rel. Emb.")
     graph.edge("HierarchicalBERT", "BERT + RTM",
                label="+Rel. Transf.")
 
-    graph.edge("SimpleXL", "AdvancedXL", "+LSTM\n+Attn.")
+    graph.edge("SimpleXL", "AdvancedXL", label="+LSTM\n+Attn.",
+               xlabel="-Pooler")
     graph.edge("AdvancedXL", "RelationalXL", xlabel="+Rel. Emb.")
     graph.edge("AdvancedXL", "XL + RTM", label="+Rel. Transf.")
 
